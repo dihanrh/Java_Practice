ad3b962bf12e9a49cf7480bcd478b91f87c77f94
dihanrh <code.dihan@gmail.com>
2022-05-16 23:09:00 +0600

    Largest amlong 3 int

---
+++ largest.java
@@ -0,0 +1,36 @@
+import java.util.Scanner  ;
+
+public class largest {
+
+    public static int max( int a , int b ,int c )
+    {
+        if (a > b && a > c)
+        {
+            return a ;
+        }
+        else if ( b > a && b > c )
+        {
+            return b ;
+        }
+        else 
+        {
+            return c ;
+        }
+    }
+    
+    public static void main (String [] args )
+    {
+        int a , b , c ; 
+        Scanner input = new Scanner(System.in) ; 
+
+        System.out.println("Enter 3 intiger : ") ;
+        a = input.nextInt() ;
+        b = input.nextInt() ;
+        c = input.nextInt() ;
+
+        System.out.println("Largest among three is : " + max( a , b ,c )) ; 
+
+
+    }
+    
+}
